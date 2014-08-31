47c5534d3123fbbd259fda5cedb5567aec9ae04a
Andy Liu <andyhome.liu@gmail.com>
2014-08-31 20:57:06 +0900

    Add Synchronizer(Semaphore) test

---
+++ sample-common/src/test/java/org/asuki/common/javase/synchronizer/SemaphoreTest.java
@@ -0,0 +1,57 @@
+package org.asuki.common.javase.synchronizer;
+
+import static java.lang.String.format;
+import static java.lang.System.out;
+import static java.lang.Thread.currentThread;
+import static java.util.stream.IntStream.range;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+
+import org.testng.annotations.Test;
+
+public class SemaphoreTest {
+
+    private static final int MAX_THREAD_NUM = 3;
+
+    @SneakyThrows
+    @Test
+    public void test() {
+        final ExecutorService service = Executors.newCachedThreadPool();
+
+        // Limit the number of threads
+        final Semaphore sp = new Semaphore(MAX_THREAD_NUM);
+
+        range(0, 5).forEach(m -> service.execute(new Task(sp)));
+
+        TimeUnit.SECONDS.sleep(10);
+    }
+
+    @AllArgsConstructor
+    private static class Task implements Runnable {
+
+        private Semaphore sp;
+
+        @SneakyThrows
+        @Override
+        public void run() {
+            sp.acquire();
+
+            out.println(format("%s entered! Current threads:%d",
+                    currentThread().getName(),
+                    (MAX_THREAD_NUM - sp.availablePermits())));
+
+            TimeUnit.SECONDS.sleep((int) (Math.random() * 10));
+
+            sp.release();
+            out.println(format("%s left! Current threads:%d", currentThread()
+                    .getName(), (MAX_THREAD_NUM - sp.availablePermits())));
+        }
+
+    }
+}
