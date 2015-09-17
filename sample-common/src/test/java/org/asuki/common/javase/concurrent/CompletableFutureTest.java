8cac754f78009d5563338097edffdd225db69b41
Andy Liu <andyhome.liu@gmail.com>
2015-09-18 01:19:00 +0900

    Add test about CompletableFuture

--- sample-common/src/test/java/org/asuki/common/javase/concurrent/CompletableFutureTest.java
+++ sample-common/src/test/java/org/asuki/common/javase/concurrent/CompletableFutureTest.java
@@ -6,6 +6,7 @@ import static java.util.concurrent.CompletableFuture.supplyAsync;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import org.testng.annotations.BeforeMethod;
@@ -21,20 +22,73 @@ public class CompletableFutureTest {
     }
 
     @Test
+    public void testBasic1() throws Exception {
+        CompletableFuture<Integer> future1 = supplyAsync(() -> {
+            if (false) {
+                throw new RuntimeException();
+            }
+            return 40;
+        });
+
+        CompletableFuture<Integer> future2 = future1.thenApply(r -> r * r)
+                .exceptionally(e -> 2);
+
+        System.out.println(future2.join()); // 1600 2
+
+        CompletableFuture<Integer> future3 = future1
+                .handle((r, e) -> e == null ? r * r : 2);
+
+        System.out.println(future3.get()); // 1600 2
+
+        CompletableFuture<Integer> future4 = future1
+                .whenComplete((r, e) -> System.out.println(r + 10)); // 50
+
+        System.out.println(future4.get()); // 40
+
+        CompletableFuture<Integer> future5 = future1
+                .thenCompose(v -> supplyAsync(() -> v + 20));
+
+        System.out.println(future5.get()); // 60
+    }
+
+    @Test
+    public void testBasic2() throws Exception {
+        CompletableFuture<Integer> future1 = supplyAsync(() -> {
+            try {
+                TimeUnit.MILLISECONDS.sleep(100);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            return 40;
+        });
+
+        CompletableFuture<Integer> future2 = supplyAsync(() -> 20);
+
+        CompletableFuture<Integer> future3 = future1.applyToEither(future2,
+                r -> r * r);
+        System.out.println(future3.get()); // 20 * 20
+
+        CompletableFuture<Integer> future4 = future1.thenCombine(future2, (r1,
+                r2) -> r1 + r2);
+        System.out.println(future4.get()); // 40 + 20
+
+        CompletableFuture<Void> future5 = future1.thenAcceptBoth(future2, (r1,
+                r2) -> System.out.println(r1 + r2 + 10)); // 40 + 20 + 10
+
+        System.out.println(future5.get()); // null
+    }
+
+    @Test
     public void testThenApply() {
-        list.stream()
-                .map(s -> supplyAsync(() -> s.toLowerCase()))
-                .map(f -> f.thenApply(n -> n + n))
-                .map(f -> f.join())
+        list.stream().map(s -> supplyAsync(() -> s.toLowerCase()))
+                .map(f -> f.thenApply(n -> n + n)).map(f -> f.join())
                 .forEach(out::println);
     }
 
     @Test
     public void testThenAccept() {
-        list.stream()
-                .map(s -> supplyAsync(() -> s.toLowerCase()))
-                .map(f -> f.thenAccept(out::println))
-                .map(f -> f.join())
+        list.stream().map(s -> supplyAsync(() -> s.toLowerCase()))
+                .map(f -> f.thenAccept(out::println)).map(f -> f.join())
                 .count();
     }
 
@@ -42,21 +96,20 @@ public class CompletableFutureTest {
     public void testWhenComplete() {
         list.stream()
                 .map(s -> supplyAsync(() -> s.toLowerCase()))
-                .map(f -> f.whenComplete((rlt, err) -> out.println(rlt + " Error:" + err)))
-                .count();
+                .map(f -> f.whenComplete((rlt, err) -> out.println(rlt
+                        + " Error:" + err))).count();
     }
 
     @Test
     public void testGetNow() {
-        list.stream()
-                .map(s -> supplyAsync(() -> s.toLowerCase()))
-                .map(f -> f.getNow("Not Done"))
-                .forEach(out::println);
+        list.stream().map(s -> supplyAsync(() -> s.toLowerCase()))
+                .map(f -> f.getNow("Not Done")).forEach(out::println);
     }
 
     @Test
     public void testThenCompose() throws Exception {
-        List<String> sites = asList("www.google.com", "www.youtube.com", "www.yahoo.com");
+        List<String> sites = asList("www.google.com", "www.youtube.com",
+                "www.yahoo.com");
 
         List<CompletableFuture<String>> futures = sites.stream()
                 .map(site -> supplyAsync(() -> download(site)))
